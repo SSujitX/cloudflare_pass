afc2a8ab7066ab5615dbf38f4d56e0caab1afd45
Sujit Biswas <ssujitxx@gmail.com>
2025-08-26 16:38:20 +0600

    refactor(captcha-handling): improve captcha handling and output files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from seleniumbase_test import SB
+from seleniumbase import SB
 import sys
 import os
 
@@ -11,15 +11,14 @@ with SB(uc=True, xvfb=True) as sb:
     )
     sb.sleep(10)
     sb.cdp.save_screenshot("before.png")
+    sb.sleep(5)
     sb.uc_gui_click_captcha()
     sb.sleep(5)
-    sb.uc_gui_handle_captcha()
-    sb.sleep(10)
     sb.cdp.save_screenshot("after.png")
     sb.cdp.sleep(2)
+    page_source = sb.cdp.get_page_source()
+    with open("content.html", "w", encoding="utf-8") as f:
+        f.write(page_source)
     cookies = sb.cdp.get_all_cookies()
-    with open("cf_clearance.txt", "w") as f:
-        for cookie in cookies:
-            # print(cookie["name"])
-            if cookie.name == "cf_clearance":
-                f.write(cookie.value)
+    with open("cf_clearance.json", "w", encoding="utf-8") as f:
+        json.dump(cookies, f, indent=4)
--- seleniumbase_test.py
+++ seleniumbase_test.py
@@ -6,16 +6,19 @@ root_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 os.chdir(root_path)
 
 with SB(uc=True, xvfb=True) as sb:
-    sb.activate_cdp_mode("https://pikbest.com/")
+    sb.activate_cdp_mode(
+        "https://www.legacy.com/api/_frontend/localmarket/united-states/california/subregion/alameda-county"
+    )
     sb.sleep(10)
     sb.cdp.save_screenshot("before.png")
+    sb.sleep(5)
     sb.uc_gui_click_captcha()
-    sb.sleep(10)
+    sb.sleep(5)
     sb.cdp.save_screenshot("after.png")
     sb.cdp.sleep(2)
+    page_source = sb.cdp.get_page_source()
+    with open("content.html", "w", encoding="utf-8") as f:
+        f.write(page_source)
     cookies = sb.cdp.get_all_cookies()
-    with open("cf_clearance.txt", "w") as f:
-        for cookie in cookies:
-            # print(cookie["name"])
-            if cookie.name == "cf_clearance":
-                f.write(cookie.value)
+    with open("cf_clearance.json", "w", encoding="utf-8") as f:
+        json.dump(cookies, f, indent=4)
